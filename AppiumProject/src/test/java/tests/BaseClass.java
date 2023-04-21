09d0f4414886122754bee8f7d67558ab8da907e0
muguntha.r <muguntha.r@kgisl.com>
2023-04-21 10:16:46 +0530

    initial commit

---
+++ AppiumProject/src/test/java/tests/BaseClass.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseClass {
+	
+	
+		public void setUp() {
+			DesiredCapabilities cap=new DesiredCapabilities();
+			
+			//desired capabilities always key and value pair
+			cap.setCapability( "platformName","Android");
+			cap.setCapability("platformVersion","12.0.5");
+			cap.setCapability("UDID", "94972e3d");;
+			cap.setCapability("deviceName", "Muguntha");
+			cap.setCapability("appPackage","com.miui.calculator" );
+			cap.setCapability("appActivity", "com.miui.calculator.cal.CalculatorActivity");
+		}
+		
+}
